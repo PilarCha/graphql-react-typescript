0815ae9f0f0ed107cec5e07ef16820c0407f2475
PilarCha <pchavez92@gmail.com>
2021-10-09 12:54:47 -0700

    Incorporated the index.tsx component to run our first data fetch from useLaunchListQuery

--- src/components/LaunchList/index.tsx
+++ src/components/LaunchList/index.tsx
@@ -1,1 +1,19 @@
-export {}
+import * as React from 'react';
+import { useLaunchListQuery } from '../../generated/graphql';
+import LaunchList from './Launchlist';
+
+const LaunchListContainer = () => {
+    const { data, error, loading } = useLaunchListQuery();    
+
+    if(loading) {
+        return <div>Loading...</div>
+    }
+
+    if(error || !data) {
+        return <div>ERROR</div>
+    }
+
+    return <LaunchList data={data} />
+    
+};
+export default LaunchListContainer;
