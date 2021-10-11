ef37b701872a659b34cf31a8034fcc3f4a7a9672
PilarCha <pchavez92@gmail.com>
2021-10-11 12:51:11 -0700

    Forgot to include the ownProps into the LaunchList indes.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import './App.css';
 
 const App = () => {
   const [id, setId] = React.useState(42);
-  const handleIdChange = React.useCallback(newId => {
+  const handleIdChange = React.useCallback((newId) => {
     setId(newId);
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      <LaunchList handleIdChange={handleIdChange}/>
-      <LaunchProfile id={id}/>
+      <LaunchList handleIdChange={handleIdChange} />
+      <LaunchProfile id={id} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
